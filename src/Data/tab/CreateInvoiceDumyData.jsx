11c06c856b0b56831610cb177107bd4b05db6514
unknown <pratigya.nextzensoftech@gmail.com>
2025-11-27 14:00:16 +0530

    change in invoice

--- src/Data/tab/CreateInvoiceDumyData.jsx
+++ src/Data/tab/CreateInvoiceDumyData.jsx
@@ -3,12 +3,24 @@ import BulkRetailInvoice from '../../Components/createInvoice/BulkRetailInvoice'
 import SingleRetailMulti from '../../Components/createInvoice/SingleRetailMulti';
 import BulkRetailMulti from '../../Components/createInvoice/BulkRetailMulti';
 import CreateInvoiceCommon from '../../Components/createInvoice/CreateInvoiceCommon'; 
+import {
+  CreateRetailInvoice,
+  CreateRackInvoice,
+  CreateEssoInvoice,
+  CreateEssoOwnerInvoice,
+  CreateEssoCustomizedInvoice,
+  CreateUttramarInvoice,
+  CreateUttramarOwnerInvoice,
+  CreateUttramarCustomizedInvoice,
+  CreateMonocodeInvoice,
+  CreateTcheckInvoice,
+} from "../../api/index";
 
 export const CreateInvoiceDumyData = [
   {
     id: '1',
     label: 'Create Single Retail Invoice',
-    component: <CreateInvoiceCommon supplier_ids="1,3,4,5,7"  supplier_name="" country_id="" invoice_type="RT"  invoice_creation="weekly"  api_name="CreateRetailInvoice" btnTtitle="Create Invoice" title="Create Single Retail Invoice"/>,
+    component: <CreateInvoiceCommon supplier_ids="1,3,4,5,7"  supplier_name="" country_id="" invoice_type="RT"  invoice_creation="weekly"   api_name={CreateRetailInvoice} btnTtitle="Create Invoice" title="Create Single Retail Invoice"/>,
   },
   {
     id: '2',
