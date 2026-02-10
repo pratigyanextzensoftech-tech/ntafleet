55a16e1eb2a160606e3d27d021f1c899c6187330
rgopal101 <rgopal101@gmail.com>
2026-02-10 17:48:58 +0530

    lals d

--- src/Components/pricing/singlePricingPdf/index.jsx
+++ src/Components/pricing/singlePricingPdf/index.jsx
@@ -5,19 +5,14 @@ import { Container, Row, Col, Card, CardBody } from "reactstrap";
 import BasicTabCard from "../../UiKits/Tabs/BoostrapTabs/BasicTabCard";
 import { SinglepricingTab } from "../../../Data/tab/SinglePricingTab";
 import useSelectableColumns from "../../../Hooks/useSelectableColumns";
-import DataTableComponent from "../../Tables/DataTable/DataTableComponent";
+import DataTableComponent from '../../Tables/DataTable/DataTableComponent';
 import Swal from "sweetalert2";
 import axios from "axios";
 import {
-  ta_pricing_pdf,
-  pricing_pdf as pricing_pdf_Api,
-  love_pricing_pdf,
-  ul_pricing_pdf,
-  esso_pricing_pdf,
-  irv_pricing_pdf,
+  ta_pricing_pdf,pricing_pdf as pricing_pdf_Api,love_pricing_pdf,ul_pricing_pdf,esso_pricing_pdf,irv_pricing_pdf
 } from "../../../api";
 import usePaginatedTable from "../../../Hooks/usePagination";
-const Index = () =>  {
+const Index = () => {
   const { createColumns,selectedRows,getFilteredData } = useSelectableColumns();
 const pricingpdf = {
   id: "id",
@@ -26,12 +21,11 @@ const pricingpdf = {
   "Pricing Date": "pricing_date",
   "Supplier": "supplier",
   "Entry_Count": "entry_count",
-  "Added_By": "added_by_name",
+  "Added_By": "idby",
   "Added_On": "added_on",
-  "Mailed_By": "mailed_by",
+  "Mailed_By": "mailby",
   "Mailed_On": "mail_on",
 }   
-
 const handleDelete = ({ ids = [], deleteApi, refetch }) => {
   if (!ids.length) {
     Swal.fire("Warning", "Please select at least one record.", "warning");
@@ -60,84 +54,61 @@ const handleDelete = ({ ids = [], deleteApi, refetch }) => {
           Swal.fire("Error!", "Failed to delete record.", "error");
         });
     }
-    const stringId = ids.join(",");
-
-    Swal.fire({
-      title: "Are you sure?",
-      text: "Do you really want to delete this record?",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-      cancelButtonText: "Cancel",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        axios
-          .delete(`${deleteApi}/${stringId}`)
-          .then(() => {
-            Swal.fire("Deleted!", "Record deleted successfully.", "success");
-            refetch(); // ✅ refresh correct tab
-          })
-          .catch(() => {
-            Swal.fire("Error!", "Failed to delete record.", "error");
-          });
-      }
-    });
-  };
-  const pricingPdfData = usePaginatedTable({
-    apiUrl: pricing_pdf_Api,
-    columnsMap: pricingpdf,
   });
-  const tapetroPdf = usePaginatedTable({
-    apiUrl: ta_pricing_pdf,
-    columnsMap: pricingpdf,
-  });
-  const tapetroPdfActual = usePaginatedTable({
-    apiUrl: ta_pricing_pdf,
-    columnsMap: pricingpdf,
-  });
-  const essoPdfWithoutTax = usePaginatedTable({
-    apiUrl: esso_pricing_pdf,
-    columnsMap: pricingpdf,
-  });
-  const essoPdf = usePaginatedTable({
-    apiUrl: esso_pricing_pdf,
-    columnsMap: pricingpdf,
-  });
-  const lovePdf = usePaginatedTable({
-    apiUrl: love_pricing_pdf,
-    columnsMap: pricingpdf,
-  });
-  const lovePdfActual = usePaginatedTable({
-    apiUrl: love_pricing_pdf,
-    columnsMap: pricingpdf,
-  });
-  const ulPdf = usePaginatedTable({
-    apiUrl: ul_pricing_pdf,
-    columnsMap: pricingpdf,
-  });
-  const irvPdf = usePaginatedTable({
-    apiUrl: irv_pricing_pdf,
-    columnsMap: pricingpdf,
-  });
-
+};
+   const pricingPdfData= usePaginatedTable({
+         apiUrl: pricing_pdf_Api,
+         columnsMap: pricingpdf,
+       });
+       const tapetroPdf = usePaginatedTable({
+         apiUrl:ta_pricing_pdf ,
+         columnsMap: pricingpdf,
+       });
+       const tapetroPdfActual = usePaginatedTable({
+         apiUrl: ta_pricing_pdf,
+         columnsMap: pricingpdf,
+       });
+         const essoPdfWithoutTax = usePaginatedTable({
+         apiUrl: esso_pricing_pdf,
+         columnsMap: pricingpdf,
+       });
+        const essoPdf = usePaginatedTable({
+         apiUrl: esso_pricing_pdf,
+         columnsMap: pricingpdf,
+       });
+         const lovePdf = usePaginatedTable({
+         apiUrl: love_pricing_pdf,
+         columnsMap: pricingpdf,
+       });
+         const lovePdfActual = usePaginatedTable({
+         apiUrl: love_pricing_pdf,
+         columnsMap: pricingpdf,
+       });
+         const ulPdf = usePaginatedTable({
+         apiUrl: ul_pricing_pdf,
+         columnsMap: pricingpdf,
+       });
+        const irvPdf = usePaginatedTable({
+         apiUrl: irv_pricing_pdf,
+         columnsMap: pricingpdf,
+       });
+        
   const tabs = [
     { id: "1", label: "Flying J Pdf", data: pricingPdfData, map: pricingpdf,deleteApi:pricing_pdf_Api },
     { id: "2", label:   (
       <>
-       Ta-Petro  <strong> [Capped]</strong>
+       Ta-Petro Pdf  - <strong> [Capped]</strong>
       </>
     ), data:tapetroPdf, map:  pricingpdf,deleteApi:ta_pricing_pdf},
     { id: "3", label:  (
       <>
-       Ta-Petro <strong> [Actual]</strong>
+       Ta-Petro Pdf - <strong> [Actual]</strong>
       </>
     ), data: tapetroPdfActual, map: pricingpdf,deleteApi:ta_pricing_pdf },
    {
-     id: "4", label: " Esso (without Text) ", data: essoPdfWithoutTax, map: pricingpdf,deleteApi: esso_pricing_pdf},
+     id: "4", label: " Esso Pdf (without Text) ", data: essoPdfWithoutTax, map: pricingpdf,deleteApi: esso_pricing_pdf},
       {
-     id: "5", label: " Esso (with Text)", data: essoPdf, map: pricingpdf,deleteApi:esso_pricing_pdf },
+     id: "5", label: " Esso Pdf (with Text)", data: essoPdf, map: pricingpdf,deleteApi:esso_pricing_pdf },
      {
      id: "6", label:(
       <>
@@ -150,12 +121,11 @@ const handleDelete = ({ ids = [], deleteApi, refetch }) => {
       Love Pdf  - <strong> [Actual]</strong>
       </>
     ), data: lovePdfActual, map: pricingpdf,deleteApi:love_pricing_pdf },
-    {
-     id: "8", label: "Ultramar Pdf", data: ulPdf, map: pricingpdf,deleteApi:ul_pricing_pdf },
+   
      {
      id: "9", label: "Irving Pdf", data: irvPdf, map: pricingpdf,deleteApi:irv_pricing_pdf },
   ];
-  const pricingpdfTab = tabs.map((tab) => ({
+   const pricingpdfTab = tabs.map((tab) => ({
     id: tab.id,
     label: tab.label,
     component: (
@@ -163,19 +133,19 @@ const handleDelete = ({ ids = [], deleteApi, refetch }) => {
         title={tab.label}
         table={true}
         buttonTitle="Delete Pricing"
-        handleDelete={() =>
-          handleDelete({
-            ids: selectedRows,
-            deleteApi: tab.deleteApi, // ✅ dynamic API
-            refetch: tab.data.fetchData, // ✅ refresh correct tab
-          })
-        }
-        tableColumns={createColumns(tab.map, tab.data.data, {
-          withCheckbox: true, // ✅ show checkboxes
-          withActions: true, // ✅ show action column
-          showDownload: false, // ✅ conditionally show download
-          showDelete: false, // ❌ hide delete
-        })}
+         handleDelete={() =>
+        handleDelete({
+          ids: selectedRows,
+          deleteApi: tab.deleteApi,   // ✅ dynamic API
+          refetch: tab.data.fetchData // ✅ refresh correct tab
+        })
+      }
+    tableColumns={createColumns(tab.map, tab.data.data,{
+    withCheckbox: true,         // ✅ show checkboxes
+    withActions: true,          // ✅ show action column
+    showDownload: false,         // ✅ conditionally show download
+    showDelete: false,          // ❌ hide delete
+    })}
         tableData={getFilteredData(tab.data.data)}
         loading={tab.data.loading}
         pagination
@@ -186,17 +156,17 @@ const handleDelete = ({ ids = [], deleteApi, refetch }) => {
       />
     ),
   }));
-  const fetchers = {
-    pricingPdfData: pricingPdfData.fetchData,
-    tapetroPdf: tapetroPdf.fetchData,
-    tapetroPdfActual: tapetroPdfActual.fetchData,
-    essoPdfWithoutTax: essoPdfWithoutTax.fetchData,
-    essoPdf: essoPdf.fetchData,
-    lovePdf: lovePdf.fetchData,
-    lovePdfActual: lovePdfActual.fetchData,
-    ulPdf: ulPdf.fetchData,
-    irvingPdf: ulPdf.fetchData,
-  };
+const fetchers = {
+  pricingPdfData: pricingPdfData.fetchData,
+  tapetroPdf: tapetroPdf.fetchData,
+  tapetroPdfActual: tapetroPdfActual.fetchData,
+  essoPdfWithoutTax: essoPdfWithoutTax.fetchData,
+  essoPdf: essoPdf.fetchData,
+  lovePdf: lovePdf.fetchData,
+  lovePdfActual: lovePdfActual.fetchData,
+  ulPdf: ulPdf.fetchData,
+  irvingPdf:ulPdf.fetchData,
+};
   return (
     <Fragment>
       <Breadcrumbs parent="Pricing" title="Pricing List" />
@@ -206,7 +176,7 @@ const handleDelete = ({ ids = [], deleteApi, refetch }) => {
             <Card>
               <HeaderCard title="Create Pricing PDF" />
               <CardBody>
-                <BasicTabCard tabContent={SinglepricingTab(fetchers)} />
+                <BasicTabCard  tabContent={SinglepricingTab(fetchers)}  />
               </CardBody>
             </Card>
           </Col>
@@ -216,6 +186,7 @@ const handleDelete = ({ ids = [], deleteApi, refetch }) => {
             <Card>
               <HeaderCard title="Pricing List" />
               <CardBody>
+
                 <BasicTabCard tabContent={pricingpdfTab} />
               </CardBody>
             </Card>
@@ -227,4 +198,3 @@ const handleDelete = ({ ids = [], deleteApi, refetch }) => {
 };
 
 export default Index;
-cxcxc
